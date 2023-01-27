28f8f3deca61dd4593c15a92d74d59b55811cf1e
Shraddha <jadhavshradddha5@gmail.com>
2023-01-27 10:02:12 +0530

    Added the attendence feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int isPresent=0;
+        int isAbscent=1;
+        int checkAttendence=(int)((Math.random()*10)%2);
+        if(checkAttendence==isPresent)
+        {
+            System.out.println("Emloyee is present");
+        }
+        else
+        {
+            System.out.println("Emloyee is abscent");
+        }
     }
 }
