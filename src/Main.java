ad7848677ddeb6289cef1c0b236c29ab4e6a5ea9
Shraddha <jadhavshradddha5@gmail.com>
2023-01-28 08:23:35 +0530

    Successfully calculating daily wage

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ public class Main {
     public static void main(String[] args) {
         int isPresent=0;
         int isAbscent=1;
+        int dailySalery;
+        int workHours;
+        int salaryPerHours=20;
         int checkAttendence=(int)((Math.random()*10)%2);
         if(checkAttendence==isPresent)
         {
             System.out.println("Emloyee is present");
+            workHours=8;
         }
         else
         {
             System.out.println("Emloyee is abscent");
+            workHours=0;
         }
+        dailySalery=workHours*salaryPerHours;
+        System.out.println("Daily salary is:"+dailySalery);
     }
 }
